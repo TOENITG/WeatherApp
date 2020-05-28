203d8a2adb088e5b797f45266d68dfe3b36d416a
Tobias Englund <tobias.englund@learnet.se>
2020-05-28 09:40:37 +0200

    Release 1

--- WeatherApp/Data/BlazorAppContext.cs
+++
@@ -1,7 +0,0 @@
-﻿public static class BlazorAppContext
-{
-    /// <summary>
-    /// The IP for the current session
-    /// </summary>
-    public static string CurrentUserIP { get; set; }
-}
--- WeatherApp/Startup.cs
+++ WeatherApp/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using System.Net.Http;
 using WeatherApp.Data;
 
 namespace WeatherApp
@@ -26,8 +27,7 @@ namespace WeatherApp
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
-            services.TryAddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            services.AddSingleton<HttpClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
